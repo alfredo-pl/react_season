7729cf0356d17afdffb70587e6c2cd4f230762ed
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-25 22:07:45 -0400

    import component season display to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './components/SeasonDisplay';
 
 const App = ( ) => {
     return ( 
         <div>
             hi there!
+            <SeasonDisplay></SeasonDisplay>
         </div>
      );
 }
