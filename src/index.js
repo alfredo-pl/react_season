26fb15c91bf544e1f1954c41f895bb7ceb8547b6
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-26 10:58:06 -0400

    return component Spinner

--- src/index.js
+++ src/index.js
@@ -1,47 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './components/SeasonDisplay';
+import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
+import Spinner from './components/Spinner/Spinner';
 
 class App extends React.Component{
 
     constructor(props){
         super(props);
-
-        this.state = {lat: 40, errorMessage: ""}
-
-       
+        this.state = {lat: null, errorMessage: ""}  
     }
-    
 
     //methods lifecycle
     //this method performs an initial data load for the component
-   componentDidMount(){
-    window.navigator.geolocation.getCurrentPosition(
-        position =>this.setState({ lat: position.coords.latitude}),
-        err=> this.setState({errorMessage: err.message})
-    );
+    componentDidMount(){ 
+        window.navigator.geolocation.getCurrentPosition(
+            position =>this.setState({ lat: position.coords.latitude}),
+            err=> this.setState({errorMessage: err.message})
+        );
     }
-/*  
+
+    /*  
     componentDidUpdate(){
         console.log("My component was just updated - it rerendered!");
     } */
     //React says we have to define render
     render (){
      
-        
-               {/*  {this.state.errorMessage && !this.state.lat ? "Error :" + this.state.errorMessage : "Latitud : "+this.state.lat } */}
-            
+        /*  {this.state.errorMessage && !this.state.lat ? "Error :" + this.state.errorMessage : "Latitud : "+this.state.lat } */
            
-            if (this.state.errorMessage && !this.state.lat) {
-                return <div>Error: {this.state.errorMessage}</div>;
-            }
-            if( !this.state.errorMessage && this.state.lat){
-                return <SeasonDisplay lat={this.state.lat}/>;
-            }
-            
-            return <div>Loading!</div>;
-            
-            
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        
+        if( !this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+        
+        return <Spinner/>;        
     } 
 }
  
